745ae0592c526892307f190124d9c30ffa9ffa33
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-07-20 20:46:45 +0400

    test: add results test

---
+++ src/components/results.test.tsx
@@ -0,0 +1,65 @@
+import { render } from '@testing-library/react';
+import { test, expect, describe } from 'vitest';
+import { PROMISE_STATUS } from '../constants';
+import { Results } from './results';
+import type { IRickMortyResponse } from '../services/rick-morty';
+
+describe('test results component', () => {
+  test('pending status', async () => {
+    const status = PROMISE_STATUS.PENDING;
+    const data = null;
+
+    const { getAllByRole } = render(<Results status={status} data={data} />);
+
+    expect(getAllByRole('heading')[0]).toHaveTextContent('Loading');
+  });
+
+  test('rejected status', async () => {
+    const status = PROMISE_STATUS.REJECTED;
+    const data = null;
+
+    const { getAllByRole } = render(<Results status={status} data={data} />);
+
+    expect(getAllByRole('heading')[0]).toHaveTextContent(
+      /Failed to get characters/
+    );
+  });
+
+  test('fulfilled status', async () => {
+    const status = PROMISE_STATUS.FULFILLED;
+    const data: IRickMortyResponse = {
+      info: {
+        count: 0,
+        pages: 0,
+        next: '',
+        prev: '',
+      },
+      results: [
+        {
+          id: 13,
+          name: 'sam',
+          status: '',
+          species: '',
+          type: '',
+          gender: '',
+          origin: {
+            name: '',
+            url: '',
+          },
+          location: {
+            name: '',
+            url: '',
+          },
+          image: 'image-url',
+          episode: [],
+          url: '',
+          created: '',
+        },
+      ],
+    };
+
+    const { getAllByRole } = render(<Results status={status} data={data} />);
+
+    expect(getAllByRole('heading')[0]).toHaveTextContent('sam');
+  });
+});
