9c56a724aabc2d50a90e7a7adb4d4f8e8d741da1
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-07-24 21:48:38 +0400

    refactor: convert character card to function

--- src/components/character-card.tsx
+++ src/components/character-card.tsx
@@ -6,51 +6,45 @@ interface ICharacterCardProps {
   character: IRickMortyCharacter;
 }
 
-export class CharacterCard extends React.Component<ICharacterCardProps> {
-  render() {
-    const {
-      character: {
-        name,
-        image,
-        origin,
-        type,
-        id,
-        status,
-        created,
-        gender,
-        episode,
-        location,
-        species,
-      },
-    } = this.props;
+export const CharacterCard: React.FC<ICharacterCardProps> = ({
+  character: {
+    name,
+    image,
+    origin,
+    type,
+    id,
+    status,
+    created,
+    gender,
+    episode,
+    location,
+    species,
+  },
+}) => (
+  <article className="box cell card-result">
+    <header className="is-flex is-justify-content-space-between is-align-items-self-start">
+      <h3 className="title">{name}</h3>
+      <div>#{id}</div>
+    </header>
 
-    return (
-      <article className="box cell card-result">
-        <header className="is-flex is-justify-content-space-between is-align-items-self-start">
-          <h3 className="title">{name}</h3>
-          <div>#{id}</div>
-        </header>
+    <section className="is-flex is-align-items-self-start">
+      <figure className="image ">
+        <img src={image} alt={name} />
+      </figure>
 
-        <section className="is-flex is-align-items-self-start">
-          <figure className="image ">
-            <img src={image} alt={name} />
-          </figure>
-
-          <div className="section">
-            <CharacterCardDetail name="Species" value={species} />
-            <CharacterCardDetail name="Origin" value={origin.name} />
-            <CharacterCardDetail name="Status" value={status} />
-            <CharacterCardDetail name="Gender" value={gender} />
-            {type && <CharacterCardDetail name="Type" value={type} />}
-            <CharacterCardDetail name="Location" value={location.name} />
-            <CharacterCardDetail name="Episodes count" value={episode.length} />
-            <CharacterCardDetail
-              name="Created"
-              value={new Date(created).toLocaleString()}
-            />
-          </div>
-        </section>
-      </article>
-    );
-  }
-}
+      <div className="section">
+        <CharacterCardDetail name="Species" value={species} />
+        <CharacterCardDetail name="Origin" value={origin.name} />
+        <CharacterCardDetail name="Status" value={status} />
+        <CharacterCardDetail name="Gender" value={gender} />
+        {type && <CharacterCardDetail name="Type" value={type} />}
+        <CharacterCardDetail name="Location" value={location.name} />
+        <CharacterCardDetail name="Episodes count" value={episode.length} />
+        <CharacterCardDetail
+          name="Created"
+          value={new Date(created).toLocaleString()}
+        />
+      </div>
+    </section>
+  </article>
+);
