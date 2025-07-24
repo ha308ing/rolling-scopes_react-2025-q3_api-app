913fcc63d678cf249df3dbca13d5b7c854c341cc
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-07-24 22:40:30 +0400

    refactor: convert app to function

--- src/app.tsx
+++ src/app.tsx
@@ -1,86 +1,31 @@
-import { Component, type ChangeEventHandler } from 'react';
+import { useState, type ChangeEventHandler } from 'react';
 import { SearchControls } from './components/search-controls';
-import {
-  getRickMortyCharacterByName,
-  type IRickMortyResponse,
-} from './services/rick-morty';
 import { Results } from './components/results';
-import { LOCALSTORAGE_SEARCH_QUERY_KEY, PROMISE_STATUS } from './constants';
-import type { T_PROMISE_STATUS } from './types';
+import { PROMISE_STATUS } from './constants';
+import { useRickMorty } from './hooks/use-rick-morty';
 
-interface IAppState {
-  searchInput: string;
-  status: T_PROMISE_STATUS | null;
-  data: IRickMortyResponse | string | null;
-}
+export const App = () => {
+  const [characterName, handleApiSearch, status, data] = useRickMorty();
+  const [searchInput, setSearchInput] = useState(characterName);
 
-export class App extends Component<unknown, IAppState> {
-  state: IAppState = {
-    searchInput:
-      window.localStorage.getItem(LOCALSTORAGE_SEARCH_QUERY_KEY) ?? '',
-    status: null,
-    data: null,
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchInput(e.target.value);
   };
 
-  abortController: AbortController | null = null;
-  previousSeachQuery: string | null = null;
-
-  handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    this.setState({ searchInput: e.target.value });
+  const handleSearch = () => {
+    handleApiSearch(searchInput);
   };
 
-  handleSearch = () => {
-    const searchQuery = this.state.searchInput.trim();
+  return (
+    <main className="container section">
+      <SearchControls
+        onChange={handleChange}
+        onSearch={handleSearch}
+        value={searchInput}
+        isLoading={status === PROMISE_STATUS.PENDING}
+      />
 
-    if (searchQuery === this.previousSeachQuery) {
-      return;
-    }
-
-    this.previousSeachQuery = searchQuery;
-    window.localStorage.setItem(LOCALSTORAGE_SEARCH_QUERY_KEY, searchQuery);
-    this.getData(searchQuery);
-  };
-
-  getData = (searchQuery: string) => {
-    const [requestPromise, abortController] =
-      getRickMortyCharacterByName(searchQuery);
-
-    this.abortController = abortController;
-
-    this.setState({ status: PROMISE_STATUS.PENDING });
-
-    requestPromise.then((response) => {
-      if (response.success) {
-        this.setState({
-          status: PROMISE_STATUS.FULFILLED,
-          data: response.data,
-        });
-      } else {
-        this.setState({ status: PROMISE_STATUS.REJECTED, data: response.data });
-      }
-    });
-  };
-
-  componentDidMount() {
-    this.getData(this.state.searchInput);
-  }
-
-  componentWillUnmount() {
-    this.abortController?.abort();
-  }
-
-  render() {
-    return (
-      <main className="container section">
-        <SearchControls
-          onChange={this.handleChange}
-          onSearch={this.handleSearch}
-          value={this.state.searchInput}
-          isLoading={this.state.status === PROMISE_STATUS.PENDING}
-        />
-
-        <Results status={this.state.status} data={this.state.data} />
-      </main>
-    );
-  }
-}
+      <Results status={status} data={data} />
+    </main>
+  );
+};
