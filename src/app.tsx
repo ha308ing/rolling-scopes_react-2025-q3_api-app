6dad7765f0c399b187626a37e48137372e382d2b
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-07-27 01:26:05 +0400

    refactor: switch to data router

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { createBrowserRouter } from 'react-router';
 import { ROUTES } from './constants';
 import { Page404 } from './pages/404-page';
 import { MainPage } from './pages/main-page';
 import { AboutPage } from './pages/about-page';
 import { CharacterDetails } from './components/character-details';
+import { RouterProvider } from 'react-router';
 
-export const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={ROUTES.ROOT} element={<MainPage />}>
-        <Route
-          path={ROUTES.CHARACTER_ID_PARAM}
-          element={<CharacterDetails />}
-        />
-      </Route>
-      <Route path={ROUTES.ABOUT} element={<AboutPage />} />
-      <Route path={ROUTES.NOT_FOUND} element={<Page404 />} />
-    </Routes>
-  </BrowserRouter>
-);
+const router = createBrowserRouter([
+  {
+    path: ROUTES.ROOT,
+    element: <MainPage />,
+    children: [
+      {
+        path: ROUTES.CHARACTER_ID_PARAM,
+        element: <CharacterDetails />,
+      },
+    ],
+    errorElement: <Page404 />,
+  },
+  {
+    path: ROUTES.ABOUT,
+    element: <AboutPage />,
+  },
+]);
+
+export const App = () => <RouterProvider router={router} />;
