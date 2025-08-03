6af7e6f9e0f29a7a40f12247aa071e76e0e32ab4
ha308ing <75647984+ha308ing@users.noreply.github.com>
2025-08-03 10:50:01 +0400

    feat: add theme to page with home link

--- src/components/page-with-home-link.tsx
+++ src/components/page-with-home-link.tsx
@@ -1,6 +1,7 @@
 import type React from 'react';
 import { Link } from 'react-router';
 import { ROUTES } from '../constants';
+import { useTheme } from '../hooks/use-theme';
 
 interface IPageWithHomeLinkProps {
   title?: React.ReactNode;
@@ -10,12 +11,18 @@ interface IPageWithHomeLinkProps {
 export const PageWithHomeLink: React.FC<IPageWithHomeLinkProps> = ({
   title,
   children,
-}) => (
-  <section className="w-100 has-text-centered py-6">
-    <h1 className="title is-1">{title}</h1>
-    {children}
-    <Link to={ROUTES.ROOT}>
-      <button className="button">Go to main</button>
-    </Link>
-  </section>
-);
+}) => {
+  const theme = useTheme();
+
+  return (
+    <div className="root" data-theme={theme}>
+      <section className="w-100 has-text-centered py-6">
+        <h1 className="title is-1">{title}</h1>
+        {children}
+        <Link to={ROUTES.ROOT}>
+          <button className="button">Go to main</button>
+        </Link>
+      </section>
+    </div>
+  );
+};
